dc851a03454a4ee4422b30bf5789823e004dc109
Tessa Power <hello@tessapower.xyz>
2026-02-16 17:09:49 -0600

    Stub out directory structure

---
+++ include/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Windows
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
+// D3D12
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+// STL
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <memory>
+#include <atomic>
+#include <mutex>
---
+++ src/viewer/main.cpp
@@ -0,0 +1,17 @@
+#include "include/stdafx.h"
+
+#include "imgui.h"
+
+auto APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int) -> int {
+{
+    try
+    {
+      // Do something
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
