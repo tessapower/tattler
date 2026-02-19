62055baf11bf7de196597698eb7f44b297dde5bf
Tessa Power <hello@tessapower.xyz>
2026-02-18 23:22:20 -0600

    Implement gpu timeline bar

---
+++ include/viewer/gpu_timeline_bar.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "common/capture_types.h"
+
+namespace tattler
+{
+
+/// <summary>
+/// Bottom panel containing horizontal GPU timeline showing event durations
+/// per frame.
+/// </summary>
+class GpuTimelineBar
+{
+  public:
+    /// <summary>
+    /// Draw the panel. Should be called between ImGui::NewFrame
+    /// and ImGui::Render.
+    /// </summary>
+    void Draw(const CaptureSnapshot* snapshot, const CapturedEvent* selectedEvent);
+};
+
+} // namespace tattler
